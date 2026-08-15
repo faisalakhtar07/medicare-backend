715e6bc8661b9090fae2c16dfd295e7da9816cf4
Faisal Akhtar <akhtar.faisal707@gmail.com>
2026-08-15 13:25:26 -0700

    fixed port issue for render

--- Desktop/medicare-pharmacy/medicare-backend/server.js
+++ Desktop/medicare-pharmacy/medicare-backend/server.js
@@ -42,5 +42,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message || 'Server error' })
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 MEDICARE API running on http://localhost:${PORT}`))
